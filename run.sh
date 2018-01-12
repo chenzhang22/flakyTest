7bf61f36ecaacdc7806341c6f76b24f81c96cc26
zc <340355960@qq.com>
2018-01-12 15:05:52 +0800

    1/12/15/05

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash  
 
-(nohup ruby main.rb repo0 > output0 2>1& & )&
-(nohup ruby main.rb repo1 > output1 2>1& & )&
-(nohup ruby main.rb repo2 > output2 2>1& & )&
-(nohup ruby main.rb repo3 > output3 2>1& & )&
-(nohup ruby main.rb repo4 > output4 2>1& & )&
+(nohup ruby main.rb repo0 > output0 2>&1 & )&
+(nohup ruby main.rb repo1 > output1 2>&1 & )&
+(nohup ruby main.rb repo2 > output2 2>&1 & )&
+(nohup ruby main.rb repo3 > output3 2>&1 & )&
+(nohup ruby main.rb repo4 > output4 2>&1 & )&
